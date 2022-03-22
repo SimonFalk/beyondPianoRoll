76a8c8882cb35d452bcb8aa9f2010396ea0ca646
Simon Falk <smnfalk@gmail.com>
2022-03-22 21:29:15 +0100

    minor tests

--- od-algorithms.py
+++ od-algorithms.py
@@ -1,13 +1,19 @@
 import numpy as np
 from madmom.features.onsets import OnsetPeakPickingProcessor, CNNOnsetProcessor, SpectralOnsetProcessor, peak_picking
+import matplotlib.pyplot as plt
 
 proc = OnsetPeakPickingProcessor(threshold=0.1)
-sodf = SpectralOnsetProcessor(onset_method="high_frequency_content")
-act_fn = sodf('../DoReMir/initslurtest_vn/slurtest-all.wav')
+sodf = CNNOnsetProcessor()
+act_fn = sodf("datasets/OnsetLabeledInstr2013/development/Violin/42954_FreqMan_hoochie_violin_pt1.wav")
 
 onset_idx = proc(act_fn)
 
+plt.plot(act_fn)
+plt.show()
+
+'''
 f = open("../DoReMir/initslurtest_vn/hfc015_onsets_all-madmom.csv", "w")
 for i in range(len(onset_idx)):
     f.write(str(np.round(onset_idx[i]*0.01, decimals=2))+ "\n")
 f.close()
+'''
