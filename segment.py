0d196b09e7d8b518e3672d998d6e898eea070902
Simon Falk <smnfalk@gmail.com>
2022-03-22 21:07:38 +0100

    make into module

--- segment.py
+++ segment.py
@@ -3,14 +3,7 @@ import sys
 import pickle
 import numpy as np
 
-if len(sys.argv) == 1:
-    in_dir = '../DoReMir/initslurtest_vn/spectrogram/'
-    out_dir = '../DoReMir/initslurtest_vn/frames/'
-else:
-    in_dir = sys.argv[1]
-    out_dir = sys.argv[2]
 
-N_MARGIN = 7
 
 def segment_spectrogram(spect, n_margin):
     data = []
@@ -21,22 +14,33 @@ def segment_spectrogram(spect, n_margin):
     data = np.stack(data)
     return data
 
-for file in os.listdir(in_dir):
-    if not file.endswith(".pickle"):
-        continue
-    
-    fname = os.path.join(in_dir, file)
-    print(fname)
-    with open(fname, 'rb') as handle:
-        spects = pickle.load(handle)
-    data = []
-    for spect in spects:
-        segmented = segment_spectrogram(spect, N_MARGIN)
-        data.append(segmented)
-    data = np.stack(data, axis=-1)
-    print(data.shape)
-    out_name = os.path.join(out_dir, file)
-    
-    with open(out_name, 'wb') as handle:
-        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
+if __name__=="__main__":
+
+    if len(sys.argv) == 1:
+        in_dir = '../DoReMir/initslurtest_vn/spectrogram/'
+        out_dir = '../DoReMir/initslurtest_vn/frames/'
+    else:
+        in_dir = sys.argv[1]
+        out_dir = sys.argv[2]
+
+    N_MARGIN = 7
+
+    for file in os.listdir(in_dir):
+        if not file.endswith(".pickle"):
+            continue
+        
+        fname = os.path.join(in_dir, file)
+        print(fname)
+        with open(fname, 'rb') as handle:
+            spects = pickle.load(handle)
+        data = []
+        for spect in spects:
+            segmented = segment_spectrogram(spect, N_MARGIN)
+            data.append(segmented)
+        data = np.stack(data, axis=-1)
+        print(data.shape)
+        out_name = os.path.join(out_dir, file)
+        
+        with open(out_name, 'wb') as handle:
+            pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
