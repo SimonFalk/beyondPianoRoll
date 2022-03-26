c88b1a17f9aa1f140dbba7310ea17bdfb9521b03
Simon Falk <smnfalk@gmail.com>
2022-03-26 12:46:04 +0100

    used for testing

--- od-algorithms.py
+++ od-algorithms.py
@@ -1,17 +1,22 @@
 import numpy as np
-from madmom.features.onsets import OnsetPeakPickingProcessor, CNNOnsetProcessor, SpectralOnsetProcessor, peak_picking
+from madmom.features.onsets import CNNOnsetProcessor
 import matplotlib.pyplot as plt
+from madmom.ml.nn import NeuralNetwork
 
-proc = OnsetPeakPickingProcessor(threshold=0.1)
-sodf = CNNOnsetProcessor()
-act_fn = sodf("datasets/OnsetLabeledInstr2013/development/Violin/42954_FreqMan_hoochie_violin_pt1.wav")
+#proc = OnsetPeakPickingProcessor(threshold=0.1)
+proc = CNNOnsetProcessor()
+res = proc("datasets/OnsetLabeledInstr2013/development/Violin/42954_FreqMan_hoochie_violin_pt1.wav")
 
-onset_idx = proc(act_fn)
+debug = 0
+#onset_idx = proc(act_fn)
 
-plt.plot(act_fn)
-plt.show()
 
 '''
+np.save(
+    "results/madmomCNNOnsetProcessor/42954_FreqMan_hoochie_violin_pt1", 
+    act_fn
+)
+
 f = open("../DoReMir/initslurtest_vn/hfc015_onsets_all-madmom.csv", "w")
 for i in range(len(onset_idx)):
     f.write(str(np.round(onset_idx[i]*0.01, decimals=2))+ "\n")
