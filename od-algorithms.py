82eff8da2b1afdabc2924282e50f731bffc89a4d
Simon Falk <smnfalk@gmail.com>
2022-04-18 09:23:23 +0200

    tested threshold

--- od-algorithms.py
+++ od-algorithms.py
@@ -1,24 +1,25 @@
 import numpy as np
 from madmom.features.onsets import CNNOnsetProcessor
+from madmom.features.onsets import OnsetPeakPickingProcessor
+
 import matplotlib.pyplot as plt
 from madmom.ml.nn import NeuralNetwork
 
-#proc = OnsetPeakPickingProcessor(threshold=0.1)
-proc = CNNOnsetProcessor()
-res = proc("datasets/OnsetLabeledInstr2013/development/Violin/42954_FreqMan_hoochie_violin_pt1.wav")
 
-debug = 0
-#onset_idx = proc(act_fn)
+for t in [0.7, 0.9, 0.95]:
+    pp = OnsetPeakPickingProcessor(threshold=t, fps=100)
+    proc = CNNOnsetProcessor()
+    res = proc("datasets/initslurtest_vn/initslurtest_vn_wav/slurtest02.wav")
+    onset_idx = pp(res)
 
 
-'''
-np.save(
-    "results/madmomCNNOnsetProcessor/42954_FreqMan_hoochie_violin_pt1", 
-    act_fn
-)
 
-f = open("../DoReMir/initslurtest_vn/hfc015_onsets_all-madmom.csv", "w")
+#np.save(
+#    "results/madmomCNNOnsetProcessor/42954_FreqMan_hoochie_violin_pt1", 
+#    act_fn
+#)
+
+f = open("datasets/initslurtest_vn_old/model-output/bockmodel095-slurtest02.txt", "w")
 for i in range(len(onset_idx)):
     f.write(str(np.round(onset_idx[i]*0.01, decimals=2))+ "\n")
 f.close()
-'''
