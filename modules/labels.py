852ff6a3ed5a8fcc9983bb749057445f10853881
simonf <simonfal@kth.se>
2022-08-25 10:24:25 +0200

    utility for shuffling labels

--- modules/labels.py
+++ modules/labels.py
@@ -1,5 +1,45 @@
 import numpy as np
 
+def invert_permutation(p):
+    """Return an array s with which np.array_equal(arr[p][s], arr) is True.
+    The array_like argument p must be some permutation of 0, 1, ..., len(p)-1.
+    """
+    p = np.asanyarray(p) # in case p is a tuple, etc.
+    s = np.empty_like(p)
+    s[p] = np.arange(p.size)
+    return s
+
+s = invert_permutation(perm)
+
+def get_label_table(Xlist,marg):
+    label_table = np.array([], dtype=np.int64).reshape(2*marg+1,0)
+    cur = 0
+    for i in range(len(Xlist)):
+        label_table = np.concatenate((
+            label_table,
+            np.stack([np.arange(cur+k, cur+Xlist[i].shape[0]-marg*2+k) for k in range(2*marg+1)])
+            
+        ), 1)
+        cur+=Xlist[i].shape[0]
+    n_tot = label_table.shape[1]
+    return label_table
+
+def shuffle_frames(Xlist,ylist, label_table, marg=7, shuffle=True):
+    Xconc = np.concatenate(Xlist)
+    yconc = np.concatenate(ylist)
+    if shuffle:
+        perm = np.random.permutation(len(yconc))
+    else:
+        perm = np.arange(len(yconc))
+    print(perm)
+    Xp =np.concatenate(Xlist)[label_table[marg,:].reshape(-1).astype(int)][perm]
+    #Xp = np.concatenate(X)[label_table[marg,:].reshape(-1).astype(int)][perm]
+    yp = np.concatenate(ylist)[perm]
+    lt = label_table[:,perm]
+    return Xp, yp, perm
+    
+
+
 def get_label_vector(onset_times, length_s, fps, fuzzy=False):
     times = np.arange(0, length_s, 1/fps)
     a = np.reshape(onset_times, (-1,1))
